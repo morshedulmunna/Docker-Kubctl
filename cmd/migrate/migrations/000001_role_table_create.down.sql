fcb98bc1b490361c9709a8388c92fd1d74bd53af
morshedulmunna <morshedulmunna1@gmail.com>
2025-03-08 11:12:29 +0600

    Practice Docker & Kubctl Repo Init

--- cmd/migrate/migrations/000001_role_table_create.down.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_roles (
-    id bigserial PRIMARY KEY,
-    role_name varchar(255) NOT NULL,
-    create_at timestamp with time zone NOT NULL DEFAULT NOW(),
-    update_at timestamp with time zone NOT NULL DEFAULT NOW()
-);
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/morshedulmunna/pxomart-api
-
-go 1.23.3
-
-require (
-	github.com/go-chi/chi v1.5.5
-	github.com/lib/pq v1.10.9
-)
